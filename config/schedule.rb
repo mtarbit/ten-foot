60a803034edfc92bacd438d9ebe2db02342c6b86
Matt Tarbit <mtarbit@gmail.com>
2013-01-06 11:01:04 +0000

    Add automation for 'populate' methods.

---
+++ config/schedule.rb
@@ -0,0 +1,13 @@
+# Learn more: http://github.com/javan/whenever
+
+set :output, 'log/whenever.log'
+
+every 1.hours do
+  runner 'VideoFile.populate'
+  runner 'Movie.populate'
+  runner 'Series.populate'
+end
+
+every 30.minutes do
+  runner 'Tweet.populate'
+end
