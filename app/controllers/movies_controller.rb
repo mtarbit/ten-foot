8ed71615c864743af4cea75023123dce0d90a7ce
Matt Tarbit <mtarbit@gmail.com>
2013-01-21 17:39:10 +0000

    Order by recently added on movie & series indexes.
    It's a more useful default than alphabetical, until I get some kind of
    sorting, filtering and searching UI stubbed out.
    
    (Accidentally included the series part of this in the previous commit,
    and had pushed before I noticed it. Ho hum.)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    # @movies = Movie.joins(:video_files).group('movies.id').order('video_files.created_at DESC').to_a
-    @movies = Movie.order('created_at DESC')
+    @movies = Movie.joins(:video_files).group('movies.id').order('video_files.created_at DESC').to_a
   end
 
   def show
