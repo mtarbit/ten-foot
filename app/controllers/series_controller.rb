7b32ceb17952cd5fa701165d31d60557b98dfe38
Matt Tarbit <mtarbit@gmail.com>
2014-05-28 21:04:26 +0100

    Tweak order of seasons and episodes on series page.

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -5,7 +5,7 @@ class SeriesController < ApplicationController
 
   def show
     @series = Series.find(params[:id])
-    @video_files = @series.video_files.order(:season, :episode, :path)
+    @video_files = @series.video_files.order('season NULLS FIRST, episode NULLS FIRST, path')
   end
 
   def watched
