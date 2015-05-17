68a5c45e3ccff01dd318c85f226cea09b67109d4
Matt Tarbit <mtarbit@gmail.com>
2015-05-17 20:25:49 +0100

    Use eager loading to cut down on queries for feeds page.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,18 +1,6 @@
 class FeedsController < ApplicationController
   def index
-    @tweets = Tweet
-                .select('tweets.*, tytv.you_tube_video_id')
-                .joins('JOIN (
-                          SELECT
-                            you_tube_video_id,
-                            MIN(tweet_id) AS min_tweet_id
-                          FROM tweets_you_tube_videos
-                          GROUP BY you_tube_video_id
-                        )
-                        AS tytv
-                        ON tytv.min_tweet_id = tweets.id')
-                .order('tweets.date DESC')
-                .limit(450)
+    @videos = YouTubeVideo.includes(:tweets).order('tweets.date DESC').limit(120)
   end
 
   def show
